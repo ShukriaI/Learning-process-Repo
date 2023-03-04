222adff7872e8e681078b624ce32d63fd5da3ab5
Shukria.I <iqbal_shukria@hotmail.com>
2023-03-04 14:57:54 -0500

    new file

---
+++ src/gitHubRepo/GitRepository.java
@@ -0,0 +1,20 @@
+package gitHubRepo;
+
+public class GitRepository {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String a = "test and it is a very bad day today ";
+		System.out.println(a);
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
